2ed88b4514b0174807abd12f0009d4167dc031e7
Rossall Sandford <motd18@hotmail.com>
2019-03-07 12:43:57 +0000

    Maps

--- js/google-map.js
+++ js/google-map.js
@@ -1,57 +1,14 @@
 
 var google;
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var myLatlng = new google.maps.LatLng(51.029119, -3.042820);
-
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 9,
-
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
-
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [
-            {
-                "featureType": "administrative.country",
-                "elementType": "geometry",
-                "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
-                ]
-            }
-        ]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['Creech St Michael'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
-    }
-    
+function initMap() {
+    // The location of Uluru
+    var hopkinsField = { lat: 51.029119, lng: -3.042820 };
+    // The map, centered at HF
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 8, center: hopkinsField });
+    // The marker, positioned at HF
+    var marker = new google.maps.Marker({ position: hopkinsField, map: map });
 }
-google.maps.event.addDomListener(window, 'load', init);
+
+google.maps.event.addDomListener(window, 'load', initMap);
